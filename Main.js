66988d73eef45ee2103d0d5962232dd1b30415db
Shishir Namdev <shishir.namdev@weenggs.in>
2025-08-05 11:05:08 +0530

    try to make viewall button dynamically

--- Main.js
+++ Main.js
@@ -1,16 +1,10 @@
-let loadMoreBtn = document.querySelector(".viewall");
-let currentItem = 3;
-
-loadMoreBtn.onclick = () => {
-  let boxes = [...document.querySelectorAll(".cloth_Section .cloth")];
-
-  for (var i = currentItem; i < currentItem + 3; i++) {
-    boxes[i].style.display = "inline-block";
-  }
-
-  currentItem += 3;
-
-  if (currentItem >= boxes.length) {
-    loadMoreBtn.style.display = "none";
-  }
-};
+$(document).ready(function () {
+  $(".products").slice(0, 2).show();
+  $("#viewmore").on("click", function (e) {
+    e.preventDefault();
+    $(".products:hidden").slice(0, 2).slideDown();
+    if ($(".products:hidden").length == 0) {
+      $("#viewmore").text("No Content").addClass("noContent");
+    }
+  });
+});
